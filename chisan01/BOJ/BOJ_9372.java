1a906fd1a23d066dd70256856d7827f2c7342595
Chisan Ahn <chisan2001@gmail.com>
2022-03-19 12:52:12 +0900

    #7 - [BOJ] Silver3 Flying Safely / 11분(성공)

---
+++ chisan01/BOJ/BOJ_9372.java
@@ -0,0 +1,21 @@
+package BOJ;
+
+// [백준] Silver3 Flying Safely
+
+import java.util.Scanner;
+
+public class BOJ_9372 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int t = 0; t < T; t++) {
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+            for (int m = 0; m < M; m++) {
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+            }
+            System.out.println(N - 1);
+        }
+    }
+}
